586590242b60fc8bb01f1a0b15c2ff6f245d9bec
Kaylah <kaylah.love311@gmail.com>
2025-10-23 18:37:38 -0400

    Make less stuttery

--- commands/play.py
+++ commands/play.py
@@ -22,7 +22,6 @@ class Play(commands.Cog):
             await ctx.send("You are not connected to a voice channel")
             return
         
-        # Create message before potentially connecting to channel
         message = await ctx.send(f"Searching for `{query}`")
         channel = ctx.author.voice.channel
         voice_client = ctx.guild.voice_client
@@ -32,43 +31,51 @@ class Play(commands.Cog):
         elif voice_client.channel != channel:
             await voice_client.move_to(channel)
         
-        # Determine URL
         url = query
         if not is_valid_youtube_url(query):
-            urls = search_youtube_urls(query, 1)
+            # Run blocking search_youtube_urls in a separate thread
+            urls = await asyncio.to_thread(search_youtube_urls, query, 1) 
+            
             if urls and len(urls) > 0:
                 url = urls[0]
             else:
                 await message.edit(content=f"Could not find a valid youtube video from your query `{query}`")
                 return
             
-        # Get video info
-        info = vidInfo if vidInfo is not None else get_video_info(url)
+        info = vidInfo
+        if vidInfo is None:
+            # Run blocking get_video_info in a separate thread
+            info = await asyncio.to_thread(get_video_info, url)
         
-        # Handle queuing
         if not forcePlay and (voice_client.is_playing() or voice_client.is_paused()):
             add_to_queue(ctx.guild.id, info)
             await message.edit(content=f"Added to queue: {info['title']}")
             return
         
         async def after_playing(_):
-            await message.delete() 
+            # Make sure we don't delete the message if it was already updated by the new song
+            try:
+                await message.delete() 
+            except Exception: # Handle discord.errors.NotFound if message is gone
+                pass
+            
             if get_queue(ctx.guild.id):
                 nextInfo = pop_from_queue(ctx.guild.id)
-                # Recursively call the internal logic for the next item
+                
+                # RECURSIVE CALL: The next track will start by running blocking calls
+                # in a thread, so it shouldn't stutter the 'after_playing' callback.
                 await self._play_audio_logic(ctx, query=nextInfo['webpage_url'], vidInfo=nextInfo, forcePlay=True)
 
-        # Get the stream URL
-        stream_url = get_audio_stream_url(url)
+        # Get the stream URL - MAKE NON-BLOCKING
+        stream_url = await asyncio.to_thread(get_audio_stream_url, url)
+
         if not stream_url:
             await message.edit(content=f"Could not find a playable audio stream from {url}") 
             return
             
-        # Play the stream
         error = await play_stream(
             ctx,
             stream_url,
-            # Pass the local async function 'after_playing' into the threadsafe execution
             after=lambda e: asyncio.run_coroutine_threadsafe(after_playing(e), self.bot.loop)
         )
 
