3c068a2e43d9d012c4610c6a5edfd440f8543a0f
Kaylah <kaylah.love311@gmail.com>
2025-10-24 02:25:07 -0400

    Big updates

--- helpers/play_stream.py
+++ helpers/play_stream.py
@@ -14,15 +14,23 @@ async def play_stream(ctx, stream_url, after=None):
 
     ffmpeg_options = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn',
+        'options': (
+            '-vn '                         # No video
+            '-ar 48000 '                   # Resample to 48kHz
+            '-ac 2 '                       # Set to 2-channel stereo
+            '-af "loudnorm=I=-16:TP=-1.5:LRA=11"' # Apply loudness normalization
+        )
     }
 
     try:
         player = discord.FFmpegPCMAudio(stream_url, executable='ffmpeg', **ffmpeg_options)
+        guild_volume = 0.1
+        player_with_volume = discord.PCMVolumeTransformer(player, volume=guild_volume)
 
         if (voice_client.is_playing() or voice_client.is_paused()):
             voice_client.stop()
-        voice_client.play(player, after=after)
+
+        voice_client.play(player_with_volume, after=after)
         return None
     except Exception as e:
         print(f"Error in play_stream: {e}")
